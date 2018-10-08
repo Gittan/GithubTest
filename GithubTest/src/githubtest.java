327c3d49b7b0a9b2fe3121b0d1568b15024f90a7
antbja1 <antbja1@5CD7172DZN.thn.local>
2018-10-08 13:40:56 +0200

    test

--- GithubTest/src/githubtest.java
+++ GithubTest/src/githubtest.java
@@ -1,7 +1,21 @@
+import java.util.Arrays;
+import javax.swing.JOptionPane;
+
 
 public class githubtest {
 	public static void main(String[] args) {
-		System.out.println("bla");
-
+		
+		
+		int sekunder = 3661;
+		
+		int timmar =sekunder / 3600;
+		
+		int rest = sekunder %3600;
+		
+		int minuter = rest/60;
+		
+		int s = rest %60;
+		
+		System.out.println(timmar+":"+minuter+":"+s);
 	}
 }
